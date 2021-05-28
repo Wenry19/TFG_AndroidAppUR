57c1dd9b7f904bdc4c03cb4bff6d87193ca2165e
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-05-28 22:02:51 +0200

    començant a plantejar classes

---
+++ EasyProduction/app/src/main/java/com/upc/EasyProduction/DashBoardConnection.java
@@ -0,0 +1,65 @@
+package com.upc.EasyProduction;
+
+import android.util.Log;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class DashBoardConnection {
+
+    private final String hostname; // robot IP
+
+    private final int portNumber = 29999; // DashBoard Connection
+
+    private Socket socket;
+    private PrintWriter out; // to be able to sent encoded strings
+
+
+    DashBoardConnection(String robotIP){
+        hostname = robotIP;
+    }
+
+    public void connect(){
+        try {
+            socket = new Socket(hostname, portNumber);
+
+            out = new PrintWriter(socket.getOutputStream(), true);
+        }
+        catch (Exception e){
+            Log.d("exceptEnric", e.toString());
+        }
+    }
+
+    public void play(){
+        out.println("play"); // in theory println adds \n in the end of the string
+    }
+
+    public void pause(){
+        out.println("pause");
+    }
+
+    public void stop(){
+        out.println("stop");
+    }
+
+    public void shutdown(){
+        out.println("shutdown");
+    }
+
+    public void popup(String text){
+        out.println("popup " + text);
+    }
+
+    public void closePopup(){
+        out.println("close popup");
+    }
+
+    public void powerOn(){
+        out.println("power on");
+    }
+
+    public void powerOff(){
+        out.println("power off");
+    }
+
+}
---
+++ EasyProduction/app/src/main/java/com/upc/EasyProduction/TcpIpConnection.java
@@ -0,0 +1,77 @@
+package com.upc.EasyProduction;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+public class TcpIpConnection {
+
+    private final String hostname; // robot IP
+
+    private final int portNumber = 30001; // primary client connection via TCP/IP
+
+    private Socket socket;
+    private InputStream in;
+
+
+    TcpIpConnection(String robotIP){
+        hostname = robotIP;
+    }
+
+    public void connect(){
+        try {
+            socket = new Socket(hostname, portNumber);
+
+            in = socket.getInputStream();
+        }
+        catch (Exception e){
+            Log.d("exceptEnric", e.toString());
+        }
+    }
+
+    private void receivePackage(){
+        try {
+
+            byte package_size[] = new byte[4]; // int
+
+            if (in.available() > 0) {
+                int num_read_bytes = in.read(package_size, 0, 4);
+                if (num_read_bytes == 4) {
+
+                    int len = ByteBuffer.wrap(package_size).getInt();
+                    Log.d("info_package", "len: " + String.valueOf(len));
+
+                    int type = in.read();
+
+                    Log.d("info_package", "type: " + String.valueOf(type));
+
+                    int current_len = len - 4 - 1;
+
+                    byte body[] = new byte[current_len]; // body
+
+                    int aux = in.read(body, 0, current_len);
+
+                    Log.d("info_package", "body read bytes: " + String.valueOf(aux));
+                }
+            } else {
+                Log.d("info_package", "not available packages");
+            }
+        }
+        catch (UnknownHostException e){
+            System.out.println(e.toString());
+            Log.d("exceptEnric", e.toString());
+        }
+        catch (IOException e){
+            System.out.println(e.toString());
+            Log.d("exceptEnric", e.toString());
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            Log.d("exceptEnric", e.toString());
+        }
+    }
+}
