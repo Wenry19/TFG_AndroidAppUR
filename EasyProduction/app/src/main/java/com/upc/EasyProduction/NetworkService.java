257f3fa85e50c0eff6169de83a5bb538a4cef696
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-05-31 16:02:30 +0200

    he convertit service en foreground service via notificació, ara sembla que el servei passa a la llista blanca del SO i no mel mata

--- EasyProduction/app/src/main/java/com/upc/EasyProduction/NetworkService.java
+++ EasyProduction/app/src/main/java/com/upc/EasyProduction/NetworkService.java
@@ -1,9 +1,14 @@
 package com.upc.EasyProduction;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -11,7 +16,11 @@ import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+
 public class NetworkService extends Service {
+
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     private final IBinder binder = new MyBinder();
 
@@ -42,13 +51,38 @@ public class NetworkService extends Service {
 
         ip = intent.getStringExtra("ip");
 
+        // https://androidwave.com/foreground-service-android-example/
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Easy Production Foreground Service")
+                .setContentText("Network Service Running with IP = " + ip)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+
         player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
         player.setLooping(true);
         player.start();
-
+        
         //startTcpIpThread();
 
         return START_NOT_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 
     @Override
